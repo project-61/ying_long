ce23e20383837841f341f7c17e408558408735b8
imlyzh <enterindex@gmail.com>
2022-06-28 18:51:16 +0800

    fixed demo simulation (verilator) warning

--- test/top.v
+++ test/top.v
@@ -1,3 +1,5 @@
+// `include "add.v"
+
 module top (
 	input [32-1:0]	a,
 	input [32-1:0]	b,
@@ -8,5 +10,18 @@ module top (
 
 
 add a1 (.b (b),.a (a),.c (c));
+
+endmodule;
+
+module add (
+	input [32-1:0]	a,
+	input [32-1:0]	b,
+	output [32-1:0]	c
+	);
+
+
+	assign c = a + b;
+
+
 
 endmodule;
